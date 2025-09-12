f0ebb468901fa4a53ee392d82035dbc14ae66c0f
Luca Picci <lpicci96@gmail.com>
2025-09-12 11:28:01 +0200

    set up story 2 module

---
+++ scripts/stories/story_2.py
@@ -0,0 +1,10 @@
+"""Story 2: Health priority and government spending on health"""
+
+import pandas as pd
+from bblocks import places
+from typing import Literal
+
+from scripts.config import Paths
+from scripts.logger import logger
+from scripts.get_raw_data import fetch_ghed_data
+
